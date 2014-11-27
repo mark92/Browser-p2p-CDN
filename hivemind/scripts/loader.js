14e385d76a7544f04344ce7b51e985ddaae0859a
MarkGanusevich <0tollerance@gmail.com>
2014-11-28 00:04:16 +0200

    Added hash validation
    Now can validate received data with SHA256, if the data is deemed
    corrupt, automatic re-download from server ensues

--- hivemind/scripts/loader.js
+++ hivemind/scripts/loader.js
@@ -25,6 +25,11 @@ var sources = [
     baseUrl+"scripts/resourceManager.js",
     baseUrl+"scripts/connectionManager.js",
     baseUrl+"scripts/globalCache.js",
+    "http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/sha256.js",
+    "http://crypto-js.googlecode.com/svn/tags/3.1.2/build/components/enc-base64-min.js",
+    "http://crypto-js.googlecode.com/svn/tags/3.1.2/build/components/core-min.js",
+    "http://crypto-js.googlecode.com/svn/tags/3.1.2/build/components/enc-utf16-min.js",
+    "http://crypto-js.googlecode.com/svn/tags/3.1/build/components/lib-typedarrays.js",
     baseUrl+"scripts/main.js"
 ];
 
